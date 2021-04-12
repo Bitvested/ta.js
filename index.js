8df34c4afdb32600b3296ba98abad3c2ff85fc62
Vultwo <n.kroesen@outlook.com>
2021-04-12 19:00:51 +0200

    Renko fix

--- index.js
+++ index.js
@@ -542,14 +542,18 @@ async function ren(data, bs) {
   })(), bh = Math.ceil(data[0][0] / bs * (10 ** decimals)) / (10 ** decimals) * bs, bl = bh - bs;
   for(var i = 1; i < data.length; i++) {
     if(data[i][0] > bh + bs) {
-      re.push([bh,bh+bs,bh,bh+bh+bs]);
-      bh+=bs;
-      bl+=bs;
+      do {
+        re.push([bh,bh+bs,bh,bh+bh+bs]);
+        bh+=bs;
+        bl+=bs;
+      } while(data[i][0] > bh + bs);
     }
     if(data[i][1] < bl - bs) {
-      re.push([bh-bs,bh-bs,bl+bs,bl+bs]);
-      bh-=bs;
-      bl-=bs;
+      do {
+        re.push([bh-bs,bh-bs,bl+bs,bl+bs]);
+        bh-=bs;
+        bl-=bs;
+      } while(data[i][1] < bl - bs);
     }
   }
   return re;
