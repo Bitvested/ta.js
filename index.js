043e4577dda2bea1ddb91935b4202e9ec737d793
Vultwo <n.kroesen@outlook.com>
2021-04-13 16:27:54 +0200

    Kama fix

--- index.js
+++ index.js
@@ -313,10 +313,10 @@ async function hull(data, len) {
 }
 async function kama(data, len1, len2, len3) {
   len1 = (!len1) ? 10 : len1, len2 = (!len2) ? 2 : len2, len3 = (!len3) ? 30 : len3;
-  var ka = await module.exports.sma(data.slice(), len1);
+  var ka = await module.exports.sma(data.slice(), len1), ka = [ka[ka.length - 1]];
   for(var i = len1 + 1; i < data.length; i++) {
     var vola = 0, change = Math.abs(data[i] - data[i - len1]);
-    for(var a = 1; a < len1; a++) vola += Math.abs(data[i] - data[i - 1]);
+    for(var a = 1; a < len1; a++) vola += Math.abs(data[(i-a)] - data[(i-a)-1]);
     var sc = (change/vola * (2/(len2+1) - 2/(len3+1) + 2/(len3+1))) ** 2;
     ka.push(ka[ka.length - 1] + sc * (data[i] - ka[ka.length - 1]));
   }
