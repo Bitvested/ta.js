d423a9c67769c9761c580e9bf661b63bf7cc5c4a
Vultwo <n.kroesen@outlook.com>
2021-04-15 19:23:44 +0200

    HA fix

--- index.js
+++ index.js
@@ -526,9 +526,9 @@ async function mom_osc(data, len) {
   return osc;
 }
 async function ha(data) {
-  var ha = [(Number(data[0][1]) + Number(data[0][4])) / 2, Number(data[0][2]), Number(data[0][3]), (Number(data[0][1]) + Number(data[0][2]) + Number(data[0][3]) + Number(data[0][4])) / 4];
+  var ha = [[(Number(data[0][0]) + Number(data[0][3])) / 2, Number(data[0][1]), Number(data[0][2]), (Number(data[0][0]) + Number(data[0][1]) + Number(data[0][2]) + Number(data[0][3])) / 4]];
   for(var i = 1; i < data.length; i++) {
-    ha.push((ha[ha.length - 1][0] + ha[ha.length - 1][3]) / 2, Math.max(open, close, Number(data[i][2])), Math.min(open, close, Number(data[i][3])), (Number(data[i][1]) + Number(data[i][2]) + Number(data[i][3]) + Number(data[i][4])) / 4);
+    ha.push([(ha[ha.length - 1][0] + ha[ha.length - 1][3]) / 2, Math.max(ha[ha.length - 1][0], ha[ha.length - 1][3], data[i][1]), Math.min(ha[ha.length - 1][0], ha[ha.length - 1][3], data[i][2]), (data[i][0] + data[i][1] + data[i][2] + data[i][3]) / 4]);
   }
   return ha;
 }
