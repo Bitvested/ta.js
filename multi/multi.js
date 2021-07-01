f88455a0e2fc71d99289ad7ad38f7e566064b206
Vultwo <n.kroesen@outlook.com>
2021-07-01 08:45:35 +0200

    Multi-threading fix

--- multi/multi.js
+++ multi/multi.js
@@ -1,28 +1,30 @@
 const { Worker } = require('worker_threads');
 
 async function sim(d, length, sims, perc) {
-  var sd = [], length = (!length) ? 50 : length, sims = (!sims) ? 1000 : sims;
-  let worker = new Worker('./multi/worker.js');
-  worker.on('message', async(result) => {
-    sd.push(result);
-  });
-  worker.on('exit', async() => {
-    if(!perc) return sd;
-    var finalprojection = d.slice();
-    for(var i = d.length; i < sd[0].length; i++) {
-      sd.sort((a, b) => {
-        if(a[i] > b[i]) return 1;
-        if(a[i] < b[i]) return -1;
-        return 0;
-      });
-      finalprojection.push(sd[Math.round(sd.length*perc)][i]);
+  return new Promise(async(resolve, reject) => {
+    var sd = [], length = (!length) ? 50 : length, sims = (!sims) ? 1000 : sims;
+    let worker = new Worker(__dirname + '/worker.js');
+    worker.on('message', async(result) => {
+      sd.push(result);
+    });
+    worker.on('exit', async() => {
+      if(!perc) return resolve(sd);
+      var finalprojection = d.slice();
+      for(var i = d.length; i < sd[0].length; i++) {
+        sd.sort((a, b) => {
+          if(a[i] > b[i]) return 1;
+          if(a[i] < b[i]) return -1;
+          return 0;
+        });
+        finalprojection.push(sd[Math.round(sd.length*perc)][i]);
+      }
+      return resolve(finalprojection);
+    });
+    for(var i = 0; i < sims; i++) {
+      worker.postMessage({f: 0, d: d, length: length});
     }
-    return finalprojection;
+    worker.postMessage({f: -1});
   });
-  for(var i = 0; i < sims; i++) {
-    worker.postMessage({f: 0, d: d, length: length});
-  }
-  worker.postMessage({f: -1});
 }
 
 module.exports = {sim};
