b8b98920595dbd235cbbaba0bd585fd9ada89abb
Vultwo <n.kroesen@outlook.com>
2021-10-26 15:18:35 +0200

    Removed ()

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ async function er(data) {
   var wins = [], losses = [], wp = 1, lp = 1;
   for(let i in data) (data[i] >= 0) ? wins.push(1+data[i]) : losses.push(1+data[i]);
   var win = (wins.length / data.length), loss = (losses.length / data.length);
-  for(let i in losses) lp *= (losses[i]);
-  for(let i in wins) wp *= (wins[i]);
+  for(let i in losses) lp *= losses[i];
+  for(let i in wins) wp *= wins[i];
   return (((wp**(1/wins.length)-1)*100) * win + ((lp**(1/losses.length)-1)*100) * loss) / 100;
 }
 async function rsi(data, len) {
