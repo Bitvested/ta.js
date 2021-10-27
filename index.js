ab7bb41c5bcc50765cf57fb67d5be1926fa617d0
Vultwo <n.kroesen@outlook.com>
2021-10-27 11:57:02 +0200

    Fixed winratio

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ async function er(data) {
 }
 async function winratio(data) {
   var wins = 0, losses = 0;
-  for(let i in data) (data[i] >= 0) ? wins += 0 : losses += 0;
+  for(let i in data) (data[i] >= 0) ? wins++ : losses++;
   return wins / (losses + wins);
 }
 async function avgwin(data) {
