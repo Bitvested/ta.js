4e8ca6d9ac45bd58b397453bf075706989993c1e
Vultwo <n.kroesen@outlook.com>
2022-03-03 21:35:57 +0100

    Worker threads require error

--- multi/multi.js
+++ multi/multi.js
@@ -1,30 +1,33 @@
-const { Worker } = require('worker_threads');
-
-async function sim(d, length, sims, perc) {
-  var sd = [], length = (!length) ? 50 : length, sims = (!sims) ? 1000 : sims;
-  return new Promise(async(resolve, reject) => {
-    let worker = new Worker(__dirname + '/worker.js');
-    worker.on('message', async(result) => {
-      sd.push(result);
-    });
-    worker.on('exit', async() => {
-      if(!perc) return resolve(sd);
-      var finalprojection = d.slice();
-      for(var i = d.length; i < sd[0].length; i++) {
-        sd.sort((a, b) => {
-          if(a[i] > b[i]) return 1;
-          if(a[i] < b[i]) return -1;
-          return 0;
-        });
-        finalprojection.push(sd[Math.round(sd.length*perc)][i]);
+try {
+  const { Worker } = require('worker_threads');
+} catch(e) {
+  return module.exports = require('../index').sim;
+} finally {
+  async function sim(d, length, sims, perc) {
+    var sd = [], length = (!length) ? 50 : length, sims = (!sims) ? 1000 : sims;
+    return new Promise(async(resolve, reject) => {
+      let worker = new Worker(__dirname + '/worker.js');
+      worker.on('message', async(result) => {
+        sd.push(result);
+      });
+      worker.on('exit', async() => {
+        if(!perc) return resolve(sd);
+        var finalprojection = d.slice();
+        for(var i = d.length; i < sd[0].length; i++) {
+          sd.sort((a, b) => {
+            if(a[i] > b[i]) return 1;
+            if(a[i] < b[i]) return -1;
+            return 0;
+          });
+          finalprojection.push(sd[Math.round(sd.length*perc)][i]);
+        }
+        return resolve(finalprojection);
+      });
+      for(var i = 0; i < sims; i++) {
+        worker.postMessage({f: 0, d: d, length: length});
       }
-      return resolve(finalprojection);
+      worker.postMessage({f: -1});
     });
-    for(var i = 0; i < sims; i++) {
-      worker.postMessage({f: 0, d: d, length: length});
-    }
-    worker.postMessage({f: -1});
-  });
+  }
+  module.exports = {sim};
 }
-
-module.exports = {sim};
