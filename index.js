69aaa056882a08a7db4a5bfb8fd0f5d662d28b77
Vultwo <n.kroesen@outlook.com>
2022-03-27 10:59:27 +0200

    Added pseudo random number generator

--- index.js
+++ index.js
@@ -951,6 +951,21 @@ async function covariance(data1, data2, length) {
 }
 const range = (min, max, rng) => Math.floor((rng) ? rng()*(Math.floor(max)-Math.ceil(min))+Math.ceil(min) : Math.random()*(Math.floor(max)-Math.ceil(min))+Math.ceil(min));
 const pick = (data, rng) => data[Math.floor((rng) ? rng()*(Math.floor(data.length)) : Math.random()*(Math.floor(data.length)))];
+function prng(seed) {
+  for(var i = 0, h = 1779033703 ^ seed.length; i < seed.length; i++) {
+    h = Math.imul(h ^ seed.charCodeAt(i), 3432918353);
+    h = h << 13 | h >>> 19;
+  }
+  h = Math.imul(h ^ (h >>> 16), 2246822507);
+  h = Math.imul(h ^ (h >>> 13), 3266489909);
+  seed = (h ^= h >>> 16) >>> 0;
+  return function() {
+    var t = seed += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+  }
+}
 module.exports = {
   aroon: { up: aroon_up, down: aroon_down, osc: aroon_osc,},
   rsi, tsi, fi, pr, stoch, atr, sma, smma, wma, vwma, ao, asi,
