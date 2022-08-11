89291a262ce870eadde29900eeba8792e595e92f
Vultwo <n.kroesen@outlook.com>
2022-08-11 09:41:09 +0200

    Fixed martingale functions

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function martingale(data, bet, max, multiplier=2) {
       current = bet;
     }
   }
-  return current;
+  return (current > max) ? max : current;
 }
 async function antimartingale(data, bet, max, multiplier=2) {
   var current = bet;
@@ -127,7 +127,7 @@ async function antimartingale(data, bet, max, multiplier=2) {
       current = bet;
     }
   }
-  return current;
+  return (current > max) ? max : current;
 }
 async function winratio(data) {
   var wins = 0, losses = 0;
@@ -1127,6 +1127,20 @@ async function cum(data, length) {
     res.push(await module.exports.sum(data.slice(i-length,i)));
   }
   return res;
+}
+async function vwwma(data, length=20) {
+  data = data.map(x=>[x[0]*x[1],x[1]]);
+  for(var i = 1, weight = 0; i <= length; i++) weight += i;
+  for(var i = length, vwma = []; i <= data.length; i++) {
+    var pl = data.slice(i-length,i);
+    var totalv = 0, totalp = 0;
+    for(q in pl) {
+      totalv += pl[q][1] * (Number(q)+1) / weight;
+      totalp += pl[q][0] * (Number(q)+1) / weight;
+    }
+    vwma.push(totalp/totalv);
+  }
+  return vwma;
 }
 module.exports = {
   aroon: { up: aroon_up, down: aroon_down, osc: aroon_osc},
