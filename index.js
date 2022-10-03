12a84428f1526e19d8d8c84ee9d8dae4c2978452
Vultwo <n.kroesen@outlook.com>
2022-10-03 18:25:16 +0200

    Std length fix

--- index.js
+++ index.js
@@ -483,6 +483,7 @@ async function variance(data, length=data.length) {
   return va;
 }
 async function std(data, length=data.length) {
+  if(length < data.length) data.splice(0,data.length-length);
   var mean = data.reduce((a, b) => a + b) / length;
   return Math.sqrt(data.reduce((sq, n) => sq + Math.pow(n - mean, 2), 0) / length);
 }
