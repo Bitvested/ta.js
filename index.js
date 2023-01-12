213ab10b3bfcb2dc059fa78ed86fc5fef04de908
Vultwo <n.kroesen@outlook.com>
2023-01-12 22:41:54 +0100

    Let to var

--- index.js
+++ index.js
@@ -906,7 +906,7 @@ const float = (min, max, rng) => (rng) ? rng()*(max-min)+min : Math.random()*(ma
 function order(data, rng) {
   var ret = []; data = data.slice();
   do {
-    let index = Math.floor((rng) ? rng()*(Math.floor(data.length)) : Math.random()*(Math.floor(data.length)));
+    var index = Math.floor((rng) ? rng()*(Math.floor(data.length)) : Math.random()*(Math.floor(data.length)));
     ret.push(data[index]);
     data.splice(index, 1);
   } while(data.length > 0);
