d61d881c25ae077bc22bb0a122342c4a07e4370f
Vultwo <n.kroesen@outlook.com>
2023-01-12 23:13:18 +0100

    Added tests

--- index.js
+++ index.js
@@ -1228,7 +1228,7 @@ async function rsi_divergence(data, length, rs) {
 async function divergence(data1, data2) {
   if(data1.length > data2.length) data1.splice(0,data1.length-data2.length)
   if(data2.length > data1.length) data1.splice(0,data2.length-data1.length)
-  for(var i = 1, out = []; i < data.length; i++) {
+  for(var i = 1, out = []; i < data1.length; i++) {
     if((data1[i] > data1[i-1] && data2[i] < data2[i-1]) || (data1[i] < data1[i-1] && data2[i] > data2[i-1])) {
       out.push(1);
     } else {
