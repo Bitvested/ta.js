67f9da4ba21372700a9b5932c16adca2f938bace
Vultwo <n.kroesen@outlook.com>
2023-01-29 11:44:37 +0100

    Fractals/pivot points, added functionality to return the pivot price

--- index.js
+++ index.js
@@ -696,14 +696,14 @@ async function chaikin_osc(data, ema1=3, ema2=10) {
   for(var i = 0; i < ef.length; i++) cha.push(ef[i]-es[i]);
   return cha;
 }
-async function fractals(data) {
-  var fractals = [[false, false], [false, false]];
+async function fractals(data, price=false) {
+  var fractals = (!price) ? [[false, false], [false, false]] : [[-1,-1],[-1,-1]];
   for(var i = 2; i < data.length-2; i++) {
-    var up = (data[i-2][0] < data[i][0] && data[i-1][0] < data[i][0] && data[i][0] > data[i+1][0] && data[i][0] > data[i+2][0]) ? true : false,
-        down = (data[i-2][1] > data[i][1] && data[i-1][1] > data[i][1] && data[i][1] < data[i+1][1] && data[i][1] < data[i+2][1]) ? true : false;
+    var up = (data[i-2][0] < data[i][0] && data[i-1][0] < data[i][0] && data[i][0] > data[i+1][0] && data[i][0] > data[i+2][0]) ? (!price) ? true : data[i][0] : (!price) ? false : -1,
+        down = (data[i-2][1] > data[i][1] && data[i-1][1] > data[i][1] && data[i][1] < data[i+1][1] && data[i][1] < data[i+2][1]) ? (!price) ? true : data[i][1] : (!price) ? false : -1;
     fractals.push([up, down]);
   }
-  fractals.push([false,false], [false, false]);
+  (!price) ? fractals.push([false,false], [false, false]) : fractals.push([[-1,-1],[-1,-1]]);
   return fractals;
 }
 async function alligator(data, jl=13, tl=8, ll=5, js=8, ts=5, ls=3) {
