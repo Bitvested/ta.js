f9c7d328ccdb56fb72c953828e110fd56c2a4134
Vultwo <n.kroesen@outlook.com>
2023-03-24 19:17:05 +0100

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,23 +1,23 @@
 const ta = require('../index.js'), assert = require('assert'), parameters = require('./parameters.js');
 for(let x in ta) {
   if(x == 'zigzag_close') {
-    ta['zigzag'].apply(null, parameters[x].in).then((res) => { try { assert.deepEqual(res, parameters[x].out); } catch(e) { console.error('Test failed @ ' + x + '\n'); } });
+    try { assert.deepEqual(ta['zigzag'].apply(null, parameters[x].in), parameters[x].out); } catch(e) { console.error('Test failed @ ' + x + '\n'); }
     continue
   }
   if(x == 'sim' || x == 'multi' || x == 'random' || x == 'fibnumbers' || x == 't_table') continue;
   if(x == 'support') {
-    ta[x].apply(null, parameters[x].in).then((res) => { try { res.calculate(9).then((res) => { try { assert.deepEqual(res, parameters[x].out); } catch(e) { console.error("Test failed @ " + x + "\n"); } }); } catch(e) { console.error("Test failed @ " + x + "\n"); } });
+    try { assert.deepEqual((ta[x].apply(null,parameters[x].in)).calculate(9), parameters[x].out); } catch(e) { console.error('Test failed @ ' + x + "\n"); }
     continue
   }
   if(x == 'resistance') {
-    ta[x].apply(null, parameters[x].in).then((res) => { try { res.calculate(4).then((res) => { try { assert.deepEqual(res, parameters[x].out); } catch(e) { console.error("Test failed @ " + x + "\n"); } }); } catch(e) { console.error("Test failed @ " + x + "\n"); } });
+    try { assert.deepEqual((ta[x].apply(null,parameters[x].in)).calculate(4), parameters[x].out); } catch(e) { console.error('Test failed @ ' + x + '\n'); }
     continue
   }
   if(x == 'aroon') {
     for(let y in ta[x]) {
-      ta[x][y].apply(null, parameters[x + '_' + y].in).then((res) => { try { assert.deepEqual(res, parameters[x + '_' + y].out); } catch(e) { console.error('Test failed @ ' + x + '_' + y); } });
+      try { assert.deepEqual(ta[x][y].apply(null,parameters[x + '_' + y].in), parameters[x + '_' + y].out); } catch(e) { console.error('Test failed @ ' + x + '_' + y); }
     }
     continue;
   }
-  ta[x].apply(null, parameters[x].in).then((res) => { try { assert.deepEqual(res, parameters[x].out); } catch(e) { console.log(res);console.error(parameters[x].out);console.error('Test failed @ ' + x + '\n'); } });
+  try { assert.deepEqual(ta[x].apply(null, parameters[x].in), parameters[x].out); } catch(e) { console.error('Test failed @ ' + x + '\n'); }
 }
