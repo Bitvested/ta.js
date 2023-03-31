2958fd7302fbe8f92879d420193de2cc49c9e108
Vultwo <n.kroesen@outlook.com>
2023-03-31 13:31:47 +0200

    Fixed halftrend

--- index.js
+++ index.js
@@ -835,52 +835,54 @@ function se(data, size) {
 }
 function halftrend(data, atrlen, amplitude, deviation) {
   var out = [], nexttrend = [0], trend = [0], up = [0], down = [0], direction = undefined;
-  for(var i = atrlen; i <= data.length; i++) {
-    var pl = data.slice(i-atrlen,i);
+  for(var i = atrlen; i < data.length; i++) {
+    var pl = data.slice(i - atrlen, i),
         maxlow = pl[pl.length-2][2],
         minhigh = pl[pl.length-2][0],
         atr2 = module.exports.atr(pl, atrlen);
-        atr2 = atr2[atr2.length-1] / 2;
+    atr2 = atr2[atr2.length - 1] / 2;
     var dev = deviation * atr2,
-        highprice = Math.max.apply(null, pl.slice(pl.length-1, pl.length).map(x=>x[0])),
-        lowprice = Math.min.apply(null, pl.slice(pl.length-1, pl.length).map(x=>x[2])),
-        highma = module.exports.sma(pl.slice(pl.length-amplitude,amplitude).map(x=>x[0]), amplitude),
-        lowma = module.exports.sma(pl.slice(pl.length-amplitude,amplitude).map(x=>x[2]), amplitude);
-    if(nexttrend[nexttrend.length-1] == 1) {
+        highprice = Math.max.apply(null, pl.map(x => x[0])),
+        lowprice = Math.min.apply(null, pl.map(x => x[2])),
+        highma = module.exports.sma(pl.slice(pl.length - amplitude).map(x => x[0]), amplitude),
+        lowma = module.exports.sma(pl.slice(pl.length - amplitude).map(x => x[2]), amplitude);
+
+    if (nexttrend[nexttrend.length - 1] == 1) {
       maxlow = Math.max(lowprice, maxlow);
-      if(highma[0] < maxlow && pl[pl.length-1][1] < pl[pl.length-2][2]) {
-        trend.push(1)
-        nexttrend.push(0)
-        minhigh = pl[pl.length-2][0]
+      if (highma[0] < maxlow && pl[pl.length - 1][1] < pl[pl.length - 2][2]) {
+        trend.push(1);
+        nexttrend.push(0);
+        minhigh = pl[pl.length - 2][0];
       }
     } else {
-      minhigh = Math.min(highprice, minhigh)
-      if(lowma[0] > minhigh && pl[pl.length-1][1] < pl[pl.length-2][0]) {
+      minhigh = Math.min(highprice, minhigh);
+      if (lowma[0] > minhigh && pl[pl.length - 1][1] < pl[pl.length - 2][0]) {
         trend.push(0);
         nexttrend.push(1);
-        maxlow = lowprice
+        maxlow = lowprice;
       }
     }
-    if(trend[trend.length-1] == 0) {
-      if(!isNaN(trend[trend.length-2]) && trend[trend.length-2] != 0) {
-        up.push((isNaN(down[down.length-2])) ? down[down.length-1] : down[down.length-2])
+
+    if (trend[trend.length - 1] == 0) {
+      if (!isNaN(trend[trend.length - 2]) && trend[trend.length - 2] != 0) {
+        up.push((isNaN(down[down.length - 2])) ? down[down.length - 1] : down[down.length - 2]);
       } else {
-        up.push((isNaN(up[up.length-2])) ? maxlow : Math.max(up[up.length-2], maxlow))
+        up.push((isNaN(up[up.length - 2])) ? maxlow : Math.max(up[up.length - 2], maxlow));
       }
       direction = 'long';
-      var atrHigh = up[up.length-1] + dev,
-          atrLow = up[up.length-1] - dev;
     } else {
-      if(!isNaN(trend[trend.length-2]) && trend[trend.length-2] != 1) {
-        down.push((isNaN(up[up.len-2])) ? up[up.length-1] : up[up.length-2])
+      if (!isNaN(trend[trend.length - 2]) && trend[trend.length - 2] != 1) {
+        down.push((isNaN(up[up.length - 2])) ? up[up.length - 1] : up[up.length - 2]);
       } else {
-        down.push((isNaN(down[down.length-2])) ? minhigh : Math.min(minhigh, down[down.length-2]))
+        down.push((isNaN(down[down.length - 2])) ? minhigh : Math.min(minhigh, down[down.length - 2]));
       }
-      direction = 'short'
-      var atrHigh = down[down.length-1] + dev,
-          atrLow = down[down.length-1] - dev
+      direction = 'short';
     }
-    out.push([atrHigh, (trend[trend.length-1] == 0) ? up[up.length-1] : down[down.length-1], atrLow, direction]);
+
+    var atrHigh = (direction === 'long') ? up[up.length - 1] + dev : down[down.length - 1] + dev,
+        atrLow = (direction === 'long') ? up[up.length - 1] - dev : down[down.length - 1] - dev;
+
+    out.push([atrHigh, (trend[trend.length - 1] == 0) ? up[up.length - 1] : down[down.length - 1], atrLow, direction]);
   }
   return out;
 }
